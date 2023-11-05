94b936787efef770fa8beb6f51c69ad90e3c3307
Sheens507 <sheens0graviti@gmail.com>
2023-11-05 15:42:08 -0500

    Update React version and add image analyzer UI components.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  // un titulo que diga algo sobre un analizador de objetos en imagenes con IA
+  return (
+    <><div className="Titulo">
+      <h1>AImagenes</h1>
+    </div>
+      // un div llamado body que contenga un input para subir la imagen
+      <div className="body">
+        <input type="text" name="file" id="file" />
+      </div>
+      // un div llamado footer que contenga un boton que diga analizar
+      <div className="footer">
+        <button>Análisis de imágene</button>
+        <button>Generación de imágene</button>
+      </div></>
+  );
 }
 
 export default App;
